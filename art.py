ac7377e02eeef0465e165c908c21013e84e5925a
Alone2 <alain.sinzig@gmail.com>
2019-06-10 00:38:46 +0200

    new color

--- art.py
+++ art.py
@@ -8,8 +8,9 @@ NOCOLOR = '\033[0m'
 GREEN = '\33[0;21m'
 YELLOW = '\33[0;33m'
 BLUE = '\33[0;34m'
+CYAN =  '\33[0;36m'
 
-ALL = [RED,NOCOLOR,GREEN,YELLOW,BLUE]
+ALL = [RED,NOCOLOR,GREEN,YELLOW,BLUE,CYAN]
 
 r = open("pics/blobber.txt", "r")
 txt = r.read()
